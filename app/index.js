669aa07922c0312b756431a45177de7601bf3de6
Alan Palazzolo <alan.palazzolo@startribune.com>
2018-11-08 12:14:38 -0600

    Initial draft. '

---
+++ app/index.js
@@ -0,0 +1,30 @@
+/**
+ * Main JS file for project.
+ */
+
+// Define globals that are added through the js.globals in
+// the config.json file, here, mostly so linting won't get triggered
+// and its a good queue of what is available:
+// /* global _ */
+
+// Dependencies
+import Content from '../templates/_index-content.svelte.html';
+import utils from './shared/utils.js';
+import cityRetail from '../assets/data/greater-mn-retail.json';
+
+// Mark page with note about development or staging
+utils.environmentNoting();
+
+// Auto enable Pym for embedding.  This will enable a Pym Child if
+// the url contains ?pym=true
+utils.autoEnablePym();
+
+// Set up
+const app = new Content({
+  target: document.querySelector('.article-lcd-body-content'),
+  data: {
+    cities: cityRetail
+  }
+});
+
+window.___app = app;
---
+++ data.js
@@ -0,0 +1,42 @@
+/**
+ * Data processing for templates
+ */
+
+// Function to send to build data
+function buildData() {
+  return {
+    cities: {
+      source: 'sources/greater-mn-retail-edited.csv',
+      local: 'greater-mn-retail.json',
+      postprocess: data => {
+        let parsed = data.map(d => {
+          const mil = 1000000;
+
+          // RCS = Retail and Consumer Services
+          return {
+            name: d['City'],
+            rcsSales2015: parseFloat(d['RCS 2015 ($mil)']) * mil,
+            rcsSales2009:
+              parseFloat(d['RCS 2009 (2015 dollars, million)']) * mil,
+            rcsSalesChange: parseFloat(d['RCS Change 2009-2015']),
+            rcsPopulation: parseInt(d['RCS Person count'], 10),
+            rcsSalesPerPerson: parseFloat(d['RCS 2015 Sales per person']),
+            rSales2015: parseFloat(d['R 2015 ($mil)']) * mil,
+            rSales2015ofRcs: parseFloat(d['R Percent of RCS']),
+            rSalesChange: parseFloat(d['R Change 2009-2015']),
+            rRetailers2015: parseInt(d['R Retailers 2015'], 10),
+            rRetailersChange: parseFloat(d['R Retailers Change 2009-2015']),
+            notes: d.Notes ? d.Notes.trim() : undefined
+          };
+        });
+
+        return parsed;
+      }
+    }
+  };
+}
+
+// Export
+module.exports = {
+  buildData
+};
